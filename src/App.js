8cf6450c4a2ca91f80866182a013a2f5acbe2599
elmtree-Ahn <clfclf3138@gmail.com>
2021-09-15 17:07:52 +0900

    header_bnr 퍼블리싱 완성

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* font */}
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+      <Header />
+    </>
   );
+}
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="bnr_header">
+          <div className="inner">
+            <a href="javascrit:voi  d(0)">
+              지금 가입하고 인기상품 &nbsp;<b>100원</b>에 받아가세요!
+              <img src="https://res.kurly.com/pc/ico/1908/ico_arrow_fff_84x84.png" alt="" />
+            </a>
+            <div className="closeBtn">
+              <img src="https://res.kurly.com/pc/ico/1908/ico_close_fff_84x84.png" alt="" />
+            </div>
+          </div>
+        </div>
+
+      </header>
+    </>
+  )
 }
 
 export default App;
