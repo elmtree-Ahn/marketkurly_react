14709578ee85151deb235781cef0010be2638705
elmtree-Ahn <clfclf3138@gmail.com>
2021-09-15 23:21:28 +0900

    userMenu 리스트 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -11,22 +12,56 @@ function App() {
   );
 }
 
+// 헤더
 function Header() {
+  let [onOffBtn, setOnOffBtn] = useState(true);
+  
+
   return (
     <>
       <header>
-        <div className="bnr_header">
-          <div className="inner">
-            <a href="javascrit:voi  d(0)">
-              지금 가입하고 인기상품 &nbsp;<b>100원</b>에 받아가세요!
-              <img src="https://res.kurly.com/pc/ico/1908/ico_arrow_fff_84x84.png" alt="" />
-            </a>
-            <div className="closeBtn">
-              <img src="https://res.kurly.com/pc/ico/1908/ico_close_fff_84x84.png" alt="" />
+        {/* bnr_header  */}
+        {
+          onOffBtn === true
+          ? (
+            <div className="bnr_header">
+              <div className="inner">
+                <a href="javascrit:void(0)">
+                  지금 가입하고 인기상품 &nbsp;<b>100원</b>에 받아가세요!
+                  <img src="https://res.kurly.com/pc/ico/1908/ico_arrow_fff_84x84.png" alt="" />
+                </a>
+                <a className="closeBtn" href="javascript:void(0)" onClick={()=>{setOnOffBtn(false)}}>
+                  <img src="https://res.kurly.com/pc/ico/1908/ico_close_fff_84x84.png" alt="" />
+                </a>
+              </div>
             </div>
+          )
+          : null
+        }
+        {/* userMenu */}
+        <div className="userMenu">
+          <div className="inner">
+            <img src="https://res.kurly.com/pc/service/common/2011/delivery_210801.png" alt="샛별 택배 배송안내" />
+            <ul className="list_menu">
+              <li className="menu">
+                <a href="javascript:void(0)" className="link_menu join">
+                  회원가입
+                </a>
+              </li>
+              <li className="menu">
+                <a href="javascript:void(0)" className="link_menu login">
+                  로그인
+                </a>
+              </li>
+              <li className="menu">
+                <a href="javascript:void(0)" className="link_menu menu_list">
+                  고객신터
+                  <img src="" alt="" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-
       </header>
     </>
   )
