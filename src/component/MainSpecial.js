66bd18e52d5d15c769ea394a6b2b34701019a492
elmtree-Ahn <clfclf3138@gmail.com>
2021-09-18 22:29:05 +0900

    MainSpecial 작성, specialItem, basicItem 컴포넌트 구분

--- src/component/MainSpecial.js
+++ src/component/MainSpecial.js
@@ -1,6 +1,36 @@
+import BasicItem from './BasicItem';
+import SpecialItem from './SpecialItem';
+
 export default function MainSpecial() {
+
+
   return(
     <>
+    
+    <div className="mainSpecial">
+      <div className="inner">
+        <div className="goodsTitle">
+          <div className="title">
+            <div className="name">
+              추석특가
+            </div>
+            <div className="name__desc">
+              소중한 분을 위한 선물, 특가로 준비하세요!
+            </div>
+          </div>
+          <div className="coment">
+            <i class="far fa-clock"></i>
+            망설이면 늦어요!
+          </div>
+        </div>
+        <div className="goodsList">
+          <ul className="list">
+            <SpecialItem />
+          </ul>
+        </div>
+      </div>
+    </div>
+    <script src="https://kit.fontawesome.com/e66717f547.js" crossorigin="anonymous"></script>
     </>
   )
 }
---
+++ src/component/SpecialItem.js
@@ -0,0 +1,32 @@
+export default function SpecialItem() {
+  
+  return (
+    <li className="special_item">
+      <a href="javascript:void(0)" className="imgBox">
+        <img src="https://img-cf.kurly.com/shop/data/goods/1629443966430l0.jpg" alt="" />
+        <div className="deadline">
+          <div className="time">14:00:34</div>
+          <span>남음</span>
+        </div>
+        <button src="https://res.kurly.com/pc/ico/2010/ico_cart.svg"></button>
+      </a>
+      <div className="infoBox">
+        <a href="javascript:void(0)" className="itemName">
+          [GNC] 랄랄라zzzzzzzzzㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+        </a>
+        <div className="salePrice">
+          <div className="sale">
+            66%
+          </div>
+          <div className="price">
+            35,000원
+          </div>
+          <div className="marketPrice">
+            40,000원
+          </div>
+        </div>
+
+      </div>
+    </li>
+  )
+}
