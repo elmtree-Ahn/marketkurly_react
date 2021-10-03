da5f8854b02791db4dca255184370c863bdef76c
elmtree-Ahn <clfclf3138@gmail.com>
2021-10-03 20:40:11 +0900

    회원가입 페이지 구조 생성

--- src/component/signup/Signup.js
+++ src/component/signup/Signup.js
@@ -8,6 +8,33 @@ export default function SignUp() {
               회원가입
             </span>
           </h3>
+          <div className="content">
+            <div className="content_inner">
+              <div className="pageSub">
+                <span className="redStar">*</span>
+                필수입력사항
+              </div>
+              <div className="line"></div>
+              <div className="signUpInputList">
+                {/* 아이디 */}
+                <div className="signUpItems">
+                  <div className="title">
+                    아이디
+                    <span className="redStar">*</span>
+                  </div>
+                  <div className="inputBox">
+                    <input type="text" />
+                    <span>6자 이상의 영문 혹은 영문과 숫자를 조합</span>
+                  </div>
+                
+                <input type="text" />
+
+              </div>
+              </div>
+
+              
+            </div>
+          </div>
         </div>
       </div>
     </>
