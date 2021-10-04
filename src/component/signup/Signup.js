4cd9700cd3c3ed3fae78b1d21317678cc96c654f
elmtree-Ahn <clfclf3138@gmail.com>
2021-10-04 16:18:30 +0900

    아이디 벨리데이션 구현

--- src/component/signup/Signup.js
+++ src/component/signup/Signup.js
@@ -2,8 +2,24 @@ import { useEffect } from 'react'
 import { useState } from 'react/cjs/react.development';
 
 export default function SignUp() {
-  let [coment, setComent] = useState(false);
+  const [coment, setComent] = useState(false);
+  const [condition, setCondition] = useState('txt');
+  const [data, setData] = useState('');
 
+  useEffect(() => {
+    if (data.length > 0 && data.length < 6 ){
+      setCondition('bad')
+    }
+    else if (data.length >= 6) {
+      setCondition('good')
+    }
+    
+  })
+
+
+  const inputData = (e) => {
+    setData(e.target.value);
+  }
   return (
     <>
       <div className="signUp" >
@@ -30,12 +46,12 @@ export default function SignUp() {
                   </div>
                   {/* 인풋박스 */}
                   <div className="inputBox">
-                    <input type="text" placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합" onClick={() => setComent(true)}/>
+                    <input type="text" placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합" onClick={() => setComent(true)} onChange={inputData}/>
                     {
                       coment === true
                       ? (
                       <div className="coment">
-                        <div className="txt">
+                        <div className={condition}>
                           6자 이상의 영문 혹은 영문과 숫자를 조합
                         </div>
                         <div className="txt">
