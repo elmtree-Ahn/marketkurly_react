ad241a8e5219f3edadd3e508d59542a4f86c2f83
elmtree-Ahn <clfclf3138@gmail.com>
2021-10-06 16:11:29 +0900

    회원가입 페이지 기능 구현

--- src/component/signup/Signup.js
+++ src/component/signup/Signup.js
@@ -1,28 +1,85 @@
 import { useEffect } from 'react'
 import { useState } from 'react/cjs/react.development';
+import styled from 'styled-components';
 
 export default function SignUp() {
   // 코멘트 보여줄까 말까
-  const [coment, setComent] = useState(false);
+  const [idComent, setIdComent] = useState(false);
+  const [pwComent, setPwComent] = useState(false);
+  const [pwChkComent, setPwChkComent] = useState(false);
+
   // 조건에 따라 값 바꾸기
-  const [condition, setCondition] = useState('txt');
+  const [idCondition, setIdCondition] = useState('txt');
+  const [pwCondition, setPwCondition] = useState('txt')
+  const [pwChkCondition, setPwChkCondition] = useState('txt')
+  const [buttonCondition, setButtonCondition] = useState('chkBtn');
+  
   // 입력 값 스테이트
   const [id, setid] = useState('');
+  const [pw, setPw] = useState('');
+  const [pwChk, setPwChk] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
 
   // 벨리데이션
   useEffect(() => {
     if (id.length > 0 && id.length < 6 ){
-      setCondition('bad')
+      setIdCondition('bad')
     }
     else if (id.length >= 6) {
-      setCondition('good')
+      setIdCondition('good')
     }
     
   }, [id])
 
+  // 비밀번호 벨리데이션
+  useEffect(() => {
+  if (pw.length > 1 && pw.length < 10 ){
+    setPwCondition('bad')
+  }
+  else if (pw.length >= 10) {
+    setPwCondition('good')
+  }
+  
+  }, [pw])
+
+  // 비밀번호 확인
+  useEffect(() => {
+  if (pwChk.length > 0 && pwChk === pw ){
+    setPwChkCondition('good')
+  }
+  else{
+    setPwChkCondition('bad')
+  }
+  
+  }, [pwChk])
+
+  // 전화번호 인증 활성화
+  useEffect(() => {
+    if (phoneNumber.length > 10) {
+      setButtonCondition('chkBtn');
+    } 
+    else {
+      setButtonCondition('noneWork');
+    }
+  })
+
   // 입력 값 변수로 받기
-  const inputData = (e) => {
+  const inputIdData = (e) => {
     setid(e.target.value);
+
+  }
+
+  const inputPwData = (e) => {
+    setPw(e.target.value);
+  }
+
+  const inputPwChkData = (e) => {
+    setPwChk(e.target.value);
+  }
+
+  const inputPhoneNumber = (e) => {
+    setPhoneNumber(e.target.value);
   }
 
   return (
@@ -45,18 +102,18 @@ export default function SignUp() {
                 {/* 아이디 */}
                 <div className="signUpItems">
                   {/* 제목 */}
-                  <div className="itemTitle">
+                  <Namestyle>
                     아이디
                     <span className="redStar">*</span>
-                  </div>
+                  </Namestyle>
                   {/* 인풋박스 */}
                   <div className="inputBox">
-                    <input type="text" placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합" onClick={() => setComent(true)} onChange={inputData}/>
+                    <InputBox type="text" placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합" onClick={() => setIdComent(true)} onChange={inputIdData}/>
                     {
-                      coment === true
+                      idComent === true
                       ? (
                       <div className="coment">
-                        <div className={condition}>
+                        <div className={idCondition}>
                           6자 이상의 영문 혹은 영문과 숫자를 조합
                         </div>
                         <div className="txt">
@@ -72,6 +129,98 @@ export default function SignUp() {
                     중복확인
                   </button>
                 </div>
+                {/* 비밀번호 */}
+                <div className="signUpItems">
+                  {/* 제목 */}
+                  <Namestyle>
+                    비밀번호
+                    <span className="redStar">*</span>
+                  </Namestyle>
+                  {/* 인풋박스 */}
+                  <div className="inputBox">
+                    <InputBox type="password" placeholder="비밀번호를 입력해주세요" onClick={() => setPwComent(true)} onChange={inputPwData}/>
+                    {
+                      pwComent === true
+                      ? (
+                      <div className="coment">
+                        <div className={pwCondition}>
+                          10자 이상의 입력
+                        </div>
+                        <div className="txt">영문/숫자/특수문자(공백 제외)만 허용하며, 2개 이상 조합</div>
+                        <div className="txt">
+                          동일한 숫자 3개 이상 연속 사용 불가
+                        </div>
+                      </div>
+                      )
+                      : null
+                    }
+                  </div>
+                </div>
+                {/* 비밀번호 확인 */}
+                <div className="signUpItems">
+                  {/* 제목 */}
+                  <Namestyle>
+                    비밀번호 확인
+                    <span className="redStar">*</span>
+                  </Namestyle>
+                  {/* 인풋박스 */}
+                  <div className="inputBox">
+                    <InputBox type="password" placeholder="비밀번호를 한번 더 입력해주세요" onClick={() => setPwChkComent(true)} onChange={inputPwChkData}/>
+                    {
+                      pwChkComent === true
+                      ? (
+                      <div className="coment">
+                        <div className={pwChkCondition}>
+                          동일한 비밀번호를 입력해주세요.
+                        </div>
+                      </div>
+                      )
+                      : null
+                    }
+                  </div>
+                </div>
+                {/* 이름 */}
+                <div className="signUpItems">
+                  {/* 제목 */}
+                  <Namestyle>
+                    이름
+                    <span className="redStar">*</span>
+                  </Namestyle>
+                  {/* 인풋박스 */}
+                  <div className="inputBox">
+                    <InputBox type="text" placeholder="이름을 입력해주세요" />
+                  </div>
+                </div>
+                {/* 이메일 */}
+                <div className="signUpItems">
+                  {/* 제목 */}
+                  <Namestyle>
+                    이메일
+                    <span className="redStar">*</span>
+                  </Namestyle>
+                  {/* 인풋박스 */}
+                  <div className="inputBox">
+                    <InputBox type="text" placeholder="예: marketcurly@kurly.com" />
+                  </div>
+                  <button className="chkBtn">
+                    중복확인
+                  </button>
+                </div>
+                {/* 휴대폰 */}
+                <div className="signUpItems">
+                  {/* 제목 */}
+                  <Namestyle>
+                    비밀번호
+                    <span className="redStar">*</span>
+                  </Namestyle>
+                  {/* 인풋박스 */}
+                  <div className="inputBox">
+                    <InputBox type="text" placeholder="숫자만 입력해주세요." onChange={inputPhoneNumber} />
+                  </div>
+                  <button className={buttonCondition}>
+                    인증번호 받기
+                  </button>
+                </div>
               </form>
 
               
@@ -82,3 +231,21 @@ export default function SignUp() {
     </>
   )
 }
+
+const Namestyle = styled.div`
+  width: 159px;
+  padding: 10px 0 0 20px;
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const InputBox = styled.input`
+  width: 332px;
+  height: 44px;
+  padding: 0 10px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  outline: none;
+  font-size: 14px;
+`;
+
