16e484227faf5613cfdfccc232a3e54ab9cb5a53
elmtree-Ahn <clfclf3138@gmail.com>
2021-10-26 20:13:40 +0900

    이미지 디버깅

--- src/component/MainEvent.js
+++ src/component/MainEvent.js
@@ -11,9 +11,9 @@ export default function MainEvent(props) {
           특가/혜택 &nbsp; <FontAwesomeIcon icon={faChevronRight} />
         </h3>
         <div className="itemWrap">
-          <EvnetItem imgUrl="https://img-cf.kurly.com/shop/data/main/3/pc_img_1631688770.jpg" />
-          <EvnetItem imgUrl="https://img-cf.kurly.com/shop/data/main/3/pc_img_1631870900.jpg" name="에델코첸 최대 40% 할인 & GIFT" desc="스텐 주방용품의 명가"/>
-          <EvnetItem imgUrl="https://img-cf.kurly.com/shop/data/main/3/pc_img_1631877997.jpg" name="추석 간편식 최대 20% 할인" desc="모둠전부터 송편까지"/>
+          <EvnetItem imgUrl="https://img-cf.kurly.com/shop/data/main/3/pc_img_1634718371.jpg" name="소중한식사 최대 15% 할인" desc="판교에서 소문난 반찬가게"/>
+          <EvnetItem imgUrl="https://img-cf.kurly.com/shop/data/main/3/pc_img_1635211546.jpg" name="미친치즈돈까스 35%!" desc="다시는 없을 최초가격 특가"/>
+          <EvnetItem imgUrl="https://img-cf.kurly.com/shop/data/main/3/pc_img_1635213095.jpg" name="컬리 단독상품 특가 최대 55%" desc="끝없는 혜택, 수퍼 세이브 워크"/>
         </div>
       </div>
     </div>
